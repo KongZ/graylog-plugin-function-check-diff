8b21463de0b4ff8e2a84fde8b2253f4d9fa61f3a
Siri <goonohc@gmail.com>
2019-01-15 16:50:14 +0700

    bump version

--- src/main/java/co/omise/graylog/plugins/checkdiff/CheckDiffFunctionMetaData.java
+++ src/main/java/co/omise/graylog/plugins/checkdiff/CheckDiffFunctionMetaData.java
@@ -33,7 +33,7 @@ public class CheckDiffFunctionMetaData implements PluginMetaData {
 
    @Override
    public Version getVersion() {
-      return Version.fromPluginProperties(getClass(), PLUGIN_PROPERTIES, "version", Version.from(1, 0, 0, "unknown"));
+      return Version.fromPluginProperties(getClass(), PLUGIN_PROPERTIES, "version", Version.from(1, 0, 1, "unknown"));
    }
 
    @Override
